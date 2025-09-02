fa07ebc403cc1fda6fc495247d5afae1ed0653fb
Pauline-Oraro <hiltrapauline@gmail.com>
2025-09-02 17:05:43 +0300

    adding routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,16 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
+
+
+//signup route
+router.post('/signup' ,signup)
+
+//login route
+router.post('/login' ,login)
+
+//logout route
+router.post('/logout' ,logout)
 
 export default router;
