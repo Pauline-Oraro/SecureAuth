eb0746adc8168012207f586e8f0defe530d8445c
Pauline-Oraro <hiltrapauline@gmail.com>
2025-09-03 18:14:56 +0300

    send welcome email functionality

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -22,3 +22,27 @@ export const sendVerificationEmail = async (email, verificationToken) => {
 		throw new Error(`Error sending verification email: ${error}`);
     }
 }
+
+//welcome email
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{email}]
+
+    try{
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "caf17cef-ac2b-4dfd-abea-43baec010dcf",
+            template_variables: {
+                company_info_name: "SecureAuth",
+                name: name
+            }
+        })
+
+        console.log("Welcome email sent successfully", response);
+    }
+
+    catch(error){
+        console.error(`Error sending welcome email`, error);
+		throw new Error(`Error sending welcome email: ${error}`);
+    }
+}
