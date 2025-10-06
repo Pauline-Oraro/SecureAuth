a26cc2a8d1e340dcddc1a5f3ba01a69c4087fc98
Pauline-Oraro <hiltrapauline@gmail.com>
2025-10-06 16:33:48 +0300

    Implemented authentication and authorization routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,51 @@
-import { Routes, Route } from "react-router-dom"
+import {Navigate, Routes, Route } from "react-router-dom"
 import FloatingShape from "./components/FloatingShape.jsx"
 import SignUpPage from "./pages/SignUpPage.jsx"
 import LoginPage from "./pages/LoginPage.jsx"
 import ForgotPasswordPage from "./pages/ForgotPasswordPage.jsx"
 import EmailVerificationPage from "./pages/EmailVerificationPage.jsx"
 import { Toaster } from "react-hot-toast"
+import { useAuthStore } from "./store/authStore.js"
+import { useEffect } from "react"
+import DashboardPage from "./pages/DashboardPage.jsx"
+import LoadingSpinner from "./components/LoadingSpinner.jsx"
+
+
+const ProtectedRoute = ({ children }) => {
+	const { isAuthenticated, user } = useAuthStore();
+
+	if (!isAuthenticated) {
+		return <Navigate to='/login' replace />;
+	}
+
+	if (!user.isVerified) {
+		return <Navigate to='/verify-email' replace />;
+	}
+
+	return children;
+};
+
+
+
+const RedirectAuthenticatedUser = ({ children }) => {
+	const { isAuthenticated, user } = useAuthStore();
+
+	if (isAuthenticated && user.isVerified) {
+		return <Navigate to='/' replace />;
+	}
+
+	return children;
+};
+
 
 function App() {
-  
+  const { isCheckingAuth, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  if(isCheckingAuth) return <LoadingSpinner />
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
@@ -37,11 +75,39 @@ function App() {
       />
 
       <Routes>
-       
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/verify-email" element={<EmailVerificationPage />} />
+
+        <Route 
+        path='/' 
+        element={<ProtectedRoute>
+							      <DashboardPage />
+						</ProtectedRoute>} 
+        />
+
+        <Route 
+        path="/signup" 
+        element={<RedirectAuthenticatedUser>
+							    <SignUpPage />
+						</RedirectAuthenticatedUser>} 
+        />
+
+        <Route 
+        path="/login" 
+        element={<RedirectAuthenticatedUser>
+							<LoginPage />
+						</RedirectAuthenticatedUser>} 
+        />
+
+        <Route path='/verify-email' element={<EmailVerificationPage />} />
+
+
+        <Route 
+        path="/forgot-password" 
+        element={<RedirectAuthenticatedUser>
+							<ForgotPasswordPage />
+						</RedirectAuthenticatedUser>} 
+        />
+
+
       </Routes>
 
       <Toaster/>
