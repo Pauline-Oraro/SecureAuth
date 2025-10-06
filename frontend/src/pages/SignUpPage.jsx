d8bd43bef560c944e84e06c76ecb7bfbaf75e73e
Pauline-Oraro <hiltrapauline@gmail.com>
2025-10-06 11:00:25 +0300

    implementing the signup functionality

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,8 +2,9 @@ import { motion } from "framer-motion"
 import Input from "../components/Input"
 import { Loader, Lock, Mail, User } from "lucide-react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
+import { useAuthStore } from "../store/authStore";
 
 
 
@@ -12,9 +13,23 @@ const SignUpPage = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    //get signup function from auth store
+    const {signup, error, isLoading} = useAuthStore()
 
     //handles submit
-    const handleSubmit = () => {}
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try{
+            await signup(email, password, name)
+            navigate('/verify-email')
+        }
+        catch(error){
+            console.log("Signup failed:", error);
+        }
+    }
 
   return (
     <motion.div 
@@ -52,15 +67,19 @@ const SignUpPage = () => {
 						onChange={(e) => setPassword(e.target.value)}
                         />
 
+                        {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
+
                         <PasswordStrengthMeter password={password} />
 
-                        <motion.button className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+                    <motion.button className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
 					   focus:ring-offset-gray-900 transition duration-200'
                         whileHover={{ scale: 1.02 }}
 						whileTap={{ scale: 0.98 }}
-						type='submit'>
-                            Sign Up
-                       </motion.button>
+						type='submit'
+                        disabled={isLoading}>
+
+                            {isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : "Sign Up"}
+                    </motion.button>
                     </form>
 
                 </div>
